2e10c322a783f2d246c5d3d523862b4a0c74229d
deonolarewaju <deonolarewaju@gmail.com>
2021-02-07 19:17:04 +0100

    fragment animations

--- app/src/main/java/com/funcrib/practice/FragmentActivity.kt
+++ app/src/main/java/com/funcrib/practice/FragmentActivity.kt
@@ -6,63 +6,108 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 class FragmentActivity : AppCompatActivity() {
+
+    /**
+    make variables global so it can be viewed outside the oncreate method
+     **/
+
+    val fragmentOne: Fragment = FirstFragment()
+    val fragmentTwo: Fragment = SecondFragment()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
 
-        val fragmentOne: Fragment = FirstFragment()
-        val fragmentTwo: Fragment = SecondFragment()
+/**
+  make variables global so it can be viewed outside the oncreate method
+  **/
+
+//        val fragmentOne: Fragment = FirstFragment()
+//        val fragmentTwo: Fragment = SecondFragment()
         val btFragmentOne: Button = findViewById(R.id.btFragmentOne)
         val btFragmentTwo: Button = findViewById(R.id.btFragmentTwo)
 
-        if (savedInstanceState == null) {
-//      Get Intent
-            val fr = intent.getStringExtra("FR")?.toInt()
+        
+/**
+  initialize default fragment
+  **/
+        initDefaultFragment()
 
-            if (fr == 10) {
-                supportFragmentManager.beginTransaction().apply {
-                    add(R.id.fragmentContainer, fragmentOne)
-                    setReorderingAllowed(true)
-                    addToBackStack(null)
-                    commit()
-                }
-            } else if (fr == 20) {
-                supportFragmentManager.beginTransaction().apply {
-                    add(R.id.fragmentContainer, fragmentTwo)
-                    setReorderingAllowed(true)
-                    addToBackStack(null)
-                    commit()
-                }
-            }
-        }
+//        Redundant
+//        if (savedInstanceState == null) {
+//      Get Intent
+//            val fr = intent.getStringExtra("FR")?.toInt()
+
+//            if (fr == 10) {
+//                supportFragmentManager.beginTransaction().apply {
+//                    add(R.id.fragmentContainer, fragmentOne)
+//                    setReorderingAllowed(true)
+//                    addToBackStack(null)
+//                    commit()
+//                }
+//            } else if (fr == 20) {
+//                supportFragmentManager.beginTransaction().apply {
+//                    add(R.id.fragmentContainer, fragmentTwo)
+//                    setReorderingAllowed(true)
+//                    addToBackStack(null)
+//                    commit()
+//                }
+//
+//            }
+//        }
+
 
         btFragmentOne.setOnClickListener {
             supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, fragmentOne)
                 setReorderingAllowed(true)
                 addToBackStack(null)
+//                call setanimations before you replace fragment
                 setCustomAnimations(
-                    R.anim.slide_in_left,
-                    R.anim.slide_out_right,
-                    R.anim.slide_in_right,
-                    R.anim.slide_out_left
+                    R.anim.enter_left_to_right,
+                    R.anim.exit_to_right,
+                    R.anim.enter_right_to_left,
+                    R.anim.exit_to_left
                 )
+//                call setanimations before you replace fragment
+                replace(R.id.fragmentContainer, fragmentOne)
                 commit()
             }
         }
         btFragmentTwo.setOnClickListener {
             supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, fragmentTwo)
                 setReorderingAllowed(true)
                 addToBackStack(null)
+//               call setanimations before you replace fragment
                 setCustomAnimations(
-                    R.anim.slide_in_left,
-                    R.anim.slide_out_right,
-                    R.anim.slide_in_right,
-                    R.anim.slide_out_left
+                    R.anim.enter_left_to_right,
+                    R.anim.exit_to_right,
+                    R.anim.enter_right_to_left,
+                    R.anim.exit_to_left
                 )
+//              call setanimations before you replace fragment
+                replace(R.id.fragmentContainer, fragmentTwo)
                 commit()
             }
         }
+    }
+
+    private fun initDefaultFragment() {
+
+        supportFragmentManager.beginTransaction().apply {
+            setReorderingAllowed(true)
+            addToBackStack(null)
+//            call set_animations before you replace fragment
+            setCustomAnimations(
+                R.anim.enter_left_to_right,
+                R.anim.exit_to_right,
+                R.anim.enter_right_to_left,
+                R.anim.exit_to_left
+            )
+//            call set_animations before you replace fragment
+            replace(R.id.fragmentContainer, fragmentOne)
+            commit()
+        }
+
     }
 }
